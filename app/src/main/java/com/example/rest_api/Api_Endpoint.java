43f83f703f53f6f3e041783872a4c40b36f570e6
Mahmudl-Hasan-870 <mahmudulhasan@Mahmuduls-MacBook-Air.local>
2024-05-27 16:50:05 +0600

    volley commit

---
+++ app/src/main/java/com/example/rest_api/Api_Endpoint.java
@@ -0,0 +1,7 @@
+package com.example.rest_api;
+
+public class Api_Endpoint {
+
+    public static String BASE_URL = "https://thelightsurprise.com/rest-api/api/";
+    public static String USERS = BASE_URL + "users";
+}
--- app/src/main/java/com/example/rest_api/MainActivity.java
+++ app/src/main/java/com/example/rest_api/MainActivity.java
@@ -1,24 +1,100 @@
 package com.example.rest_api;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private UserAdapter userAdapter;
+    private List<User> userList = new ArrayList<>();
+
+    private ProgressBar progressBar;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        progressBar = findViewById(R.id.progress);
+
+        fetchUsers();
+    }
+
+    private void fetchUsers() {
+        progressBar.setVisibility(View.VISIBLE);
+        String url = Api_Endpoint.USERS;
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        try {
+
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject jsonObject = response.getJSONObject(i);
+
+                                User user = new User();
+                                user.setName(jsonObject.getString("name"));
+                                user.setEmail(jsonObject.getString("email"));
+                                user.setPhone(jsonObject.getString("phone"));
+
+                                userList.add(user);
+                            }
+
+
+                            userAdapter = new UserAdapter(userList);
+                            recyclerView.setAdapter(userAdapter);
+
+                            progressBar.setVisibility(View.GONE);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace(); // Proper error handling
+                            progressBar.setVisibility(View.GONE);
+                            Log.d("TAG", "Error : " + e.getMessage());
+
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Error fetching data", Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
+
+        requestQueue.add(jsonArrayRequest);
     }
 }
